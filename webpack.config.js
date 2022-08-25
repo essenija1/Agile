2ff8e697d29782a2f4e617561e26e2986e1012e7
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-08-25 19:37:20 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,63 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-  entry: __dirname + "/src/app/index.js", 
-  output: {
-    path: __dirname + '/dist', 
-    filename: 'bundle.js',  
-    publicPath: '/' 
-  },
-  module: {  
-      rules: [ 
-      ]
-  },
-  plugins: [  
-      new HtmlWebpackPlugin({
-          template: __dirname + "/src/public/index.html",
-          inject: 'body'
-      })
-  ],
-  devServer: {
-    port: 3000,
-    hot: true,
-    static: {
-      directory: path.join(__dirname, './dist'),
+    context: path.join(__dirname, "src"),
+    mode: "development",
+    entry: "./js/index.js",
+    output: {
+        filename: "[contenthash].js",
+        path: path.join(__dirname, "public"),
     },
-  },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, "src/assets"),
+                to: path.resolve(__dirname, "public/assets"),
+            }, ],
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [require("autoprefixer")],
+                            },
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        compress: true,
+        port: 9000,
+    },
 };
