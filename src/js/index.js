6e0df4fdaa1509e5b793a4bd1c3886235aae1508
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-09-01 12:58:08 +0300

    Create index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import CaloriesCounter from "./modules/CaloriesCounter";
 import "../css/style.css";
 import { a } from "./modules/script";
+import * as registationJs from "../registration/index.js";
+import * as meditationJs from "../meditation/app.js";
+import * as kbjyJs from "../kbjy/index.js";
 
 console.log(a);
 
